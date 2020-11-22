6e20b3dc14d3ea2fdc6e14bfce910447e987be33
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-11-22 15:53:27 -0400

    Adicionei o campo data/hora

--- application/src/views/internal/parties/detail/components/PartyForm.vue
+++ application/src/views/internal/parties/detail/components/PartyForm.vue
@@ -12,6 +12,24 @@
         label="Descrição"
         outlined
         required/>
+      <v-layout row class="px-3">
+        <v-flex class="xs6 pr-2">
+          <v-text-field
+            type="date"
+            v-model="date"
+            label="Data (opcional)"
+            outlined
+            required/>
+        </v-flex>
+        <v-flex class="xs6 pl-2">
+          <v-text-field
+            type="time"
+            v-model="time"
+            label="Hora (opcional)"
+            outlined
+            required/>
+        </v-flex>
+      </v-layout>
       <v-layout class="px-3 mt-5" row wrap>
         <v-flex class="d-flex align-center justify-end xs12">
           <v-btn class="mr-5" depressed @click="goToPartiesPage">
@@ -29,6 +47,12 @@
 <script>
   export default {
     name: "party-form",
+    data() {
+      return {
+        date: null,
+        time: null
+      }
+    },
     props: {
       value: {
         type: Object,
@@ -37,7 +61,16 @@
     },
     methods: {
       submit() {
-        this.$emit('submit');
+        if (this.isThereACompleteDate()) {
+          this.formatDateTime();
+        }
+        this.$emit("submit");
+      },
+      isThereACompleteDate() {
+        return this.date && this.time;
+      },
+      formatDateTime() {
+        this.value.dateTime = new Date(`${this.date} ${this.time}`);
       },
       goToPartiesPage() {
         this.$router.push({ name: "parties" });
